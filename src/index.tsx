09d642473d055b8b57df9d46fcc403e4ab6393e0
irlan.freitas <irlan.freitas@hotmail.com>
2021-09-24 09:39:11 -0300

    feat: commit inicial com a estrutura base do projeto

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Dashboard from './pages/Dashboard'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Dashboard />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Transactions: React.FC = () => {
+    return <><h1>Transactions</h1></>
+}
+
+export default Transactions;
